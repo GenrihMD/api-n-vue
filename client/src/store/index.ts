430a85b3e3c9108485e485563862ba47e206144c
laamin <genrih.md@gmail.com>
2020-11-08 19:12:06 +0300

    Add fetch n rendr posts

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -5,10 +5,19 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    data: {}
   },
   mutations: {
+    SAVE_DATA: (state, playload) => {
+      state.data = playload
+    }
   },
   actions: {
+    fetchData: async ({ commit }) => {
+      const rs = await fetch('/api/data')
+      const data = await rs.json()
+      commit('SAVE_DATA', data)
+    }
   },
   modules: {
   },
