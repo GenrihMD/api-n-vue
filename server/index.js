57ee8317b3b09e8ff58d546af6e18b6df0128490
laamin <genrih.md@gmail.com>
2020-11-08 19:11:37 +0300

    Add fetching of  data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
+const fetch = require('node-fetch')
+
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({
+      true: true
+  })
 })
+
+app.get('/data', async (req, res) => {
+    const rs = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await rs.json()
+    res.json(data)
+})
+  
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
